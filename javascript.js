6fd0c7f31567594c73d85c41257cf75a775efbb9
rahulyogi6376 <deepakyogi6376@gmail.com>
2024-07-11 16:16:35 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -162,11 +162,14 @@ document.write(a+b)
 document.write('<br>'+x)
 
 
+
+
+
 // JavaScript Function
 // 1. Function Declaration vs. Function Expression
 
 // Function Declaration:
-// jab kisi funtion ko declared karte hai , to uske code ko define karne ke liye funtone ko call karwate hai
+// jab kisi funtion ko declared karte hai , to uske hosting ke karan top par bhi call karwa sakte hai
 
 function Remove(a , b) {
     return a-b
@@ -175,39 +178,191 @@ console.log(Remove(12,44))
 
 
 
+
+console.log(sweet("nice"))
+
+function sweet(n){
+return n
+}
+
+
+
 // Function Expression:
+// फ़ंक्शन एक्सप्रेशन में, एक फ़ंक्शन को वेरिएबल में स्टोर किया जाता है:
 // esme ye hai funtion ko upar call nhi kra sakte ,kyo ki ye variabale me assing ho rha hai, tabhi esko code ke niche hi call karte hai, 
 let javascript = function(a, b) {
 return a + b
 }
 console.log(javascript("Rahul", "Sanju"))
 
+var city = function (a , b) {
+    return a + b
+}
 
+console.log(city("Jaipur","Jaipur"))
 
 
 // 2. Arrow Functions
+
+// ES6 ke baad Arrow funtion ka use karke short tarike se laya gya funtion ko
 // errow funtion ko 2 tarah se likh sakte hai
 
-const student=(a , b)=>{
-    return a * b
-}
+//syntax
+
+// () => {};
+
+// singalline
+const student=(a , b)=> a * b;
+
 console.log(student(4,4))
+
+// maltiline
+const Class=(a , b)=>{
+    return a + b
+}
+console.log(Class(4,4))
 
 
 
 // Default Parameters
-// es me adhik perameters pass karwane or defauld value pass karwane me kaam aata hai ,esme agar perameters me value pass nhi karne par undefiend batata hai, 
-function myFunction(firstName="guset",lastName=""){
-    console.log(firstName,lastName)
-} 
-myFunction("rahul")
+  //Default parameters JavaScript mein function definitions mein use kiye jaate hain jab humein kisi parameter ko default value assign karna hota hai, agar function call mein wo parameter provide nahi kiya gaya hai ya undefined hai. Yeh parameters function ke definition mein assign kiye jaate hain.
+  // uper vale ex me ek function ko create kiya ki () ke andar ek pram pass kiya or default value set kar diya ab me jab function ko bina arguments kiye consol karu ga to undifend nai aye ga oske jhag par default value aa jaye gi
+
+// function myFunction(firstName="guset",lastName=""){
+//     console.log(firstName,lastName)
+// } 
+// myFunction("rahul")
+
+
+// function byDefo(as="Mr",ps="Dr") {
+//     return as+ps
+// }
+// console.log(byDefo("jai"))
 
 
 // 4. Rest Parameters
 
+// Rest Parameter me funtion ko declear karne usme parameter pass karenge jis bhi value ko nikalna hai
+  //Rest parameters JavaScript mein ek feature hai jo allow karta hai multiple arguments ko ek array mein collect karne function ke andar. Rest parameters ko define karne ke liye hum ... operator ka istemal karte hain.
 
-function sum(...numbers) {
-    return numbers.reduce((total, num) => total + num, 0);
+// function sum(first,last,...other) {
+//     return first+last+ other
+// }
+// console.log(sum( "Rahul","Sanju",1, 2, 3));
+
+
+// function nice(Name,Class,Age,other) {
+//     return `${Name},${Class},${Age},${other[2]}`
+// }
+
+// console.log(nice("Rahul","BA Second Year","Jaipur","Rajsthan","India"))
+  //is ex me ek funcation create kiya fir os me 3 pram pass kiye jo othersdeatils pram h vo yha par rest parameters h jo firstName or lastName ki value ko jodakar baki value ya aruments ko array ke rup me collect kar lete h islye firstName or lastName ke bad ki value othersDeatils me stroe ho gayi
+
+
+
+
+// 5. Destructuring Parameters
+// kisi object and arrey me se data nikalne ke liye eska use lete hai
+//JavaScript में Destructuring एक शक्तिशाली सुविधा है जो आपको arrays और objects से values को आसानी से extract (निकालने) की अनुमति देती है और उन्हें variables (चरों) में assign (सौंप) करती है। इस सुविधा का उपयोग parameter destructuring के रूप में function parameters के साथ भी किया जा सकता है।
+
+// const user = {
+//     firstName: 'Ajay',
+//     lastName: 'Yogi',
+//     age: 30
+// };
+
+// function getFullName({ firstName, lastName }) {
+//     console.log( `${firstName}, ${lastName}`)
+// }
+// getFullName(user);
+
+
+// const member = [1,2,3,4,5,6]
+// function membercount(first,last) {
+//     console.log `${member[2]},${member[5]}`
+// }
+// membercount(member)
+
+
+// let prodets = ['Lover','Tsart','sart',]
+// function apna([a,b,c]) {
+//     console.log `${a},${b}`
+// }
+// apna(prodets)
+
+
+
+
+
+// 6. Function Naming Conventions
+
+// CamelCase
+// function calculateTotal(price, tax) {
+//     return price + tax;
+// }
+
+// UparSneckCase
+// function FULL_NAME(userId) {
+//     // Fetch user data from a database or API
+// }      
+
+
+
+
+
+
+ // 7. Pure Function:
+//Pure function एक ऐसी function होती है जो अपने input parameters पर आधारित output प्रदान करती है और function के बाहर की किसी भी state को modify नहीं करती। यह functional programming में एक महत्वपूर्ण concept है।
+
+
+// function pureFuntion(x) {
+//     return x+x;
+// }
+// console.log(pureFuntion(30))
+
+
+// और function के बाहर की किसी भी state को modify नहीं करती।
+
+// let y=2;
+// function pureNum(y) {
+//     return y+y;
+// }
+// console.log(pureNum())
+
+
+
+
+
+// 8. Higher Order Function:
+//Higher Order Function  एक ऐसी function Hota है जो या तो एक या एक से अधिक functions को argument के रूप में लेती है या एक function को return करती है। JavaScript में, functions को first-class citizens माना जाता है, यानी functions को variables की तरह pass किया जा सकता है, return किया जा सकता है, और अन्य functions के अंदर use किया जा सकता है।
+
+
+function girl(myName) {
+    return ('hello ${myName}')
 }
 
-console.log(sum(1, 2, 3));
+function boy(myName) {
+    return girl(myName)
+}
+
+boy(girl,"Rahul")
+
+
+//9. error handling:
+//JavaScript में error handling एक महत्वपूर्ण aspect है जिससे आप अपने code को robust और reliable बना सकते हैं। Error handling के ज़रिए आप unexpected errors को handle कर सकते हैं और user को meaningful messages दे सकते हैं। JavaScript में error handling के लिए सबसे प्रमुख तरीका try...catch statement है।
+
+
+
+try {
+    defe(); 
+    const defe = function () {
+        console.log("hello");
+    };
+} catch (error) {
+    console.error(error.message); 
+}
+
+
+
+
+
